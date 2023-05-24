f341329592a65a78761708b23e1629c295828915
haasstt <nurafiifalmasazhr@gmail.com>
2023-05-23 21:53:43 -0700

    nih:

---
+++ sidebar.php
@@ -0,0 +1,21 @@
+<nav>
+    <div class="sidebarNav">
+        <div class="sidebarWrap">
+            <li>aaaa</li>
+            
+  <button class="navDropdown">Dropdown 
+    <i class="fa fa-caret-down"></i>
+  </button>
+  <div class="dropdown-container">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+            <li>aaaa</li>
+            <li>aaaa</li>
+            <li>aaaa</li>
+            <li>aaaa</li>
+            <li>aaaa</li>
+        </div>
+    </div>
+</nav>
