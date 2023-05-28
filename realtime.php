15031869166a41170c39d690dc92c118f535773a
Syauqi <syauqisnh@gmail.com>
2023-05-28 20:28:26 +0700

    push

--- realtime.php
+++ realtime.php
@@ -1,18 +1,3 @@
 <div class="header">
     <a>Realtime</a>
 </div>
-
-<div class="container">
-    <div class="row">
-
-        <div>
-            <div>
-                <div>
-                    <p>cucuegueubu</p>
-                    <h5>Updated</h5>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</div>
